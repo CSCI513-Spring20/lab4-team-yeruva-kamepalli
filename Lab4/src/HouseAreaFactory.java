dca96005354c3194666451891c5a06ef6c2581ca
sivak <sivak@10.0.0.196>
2020-05-01 13:32:56 -0500

    Final commit

---
+++ Lab4/src/HouseAreaFactory.java
@@ -0,0 +1,12 @@
+public class HouseAreaFactory  extends HouseFactory{
+public HouseEntity createHouse(String item)
+{
+if (item.equals("House")) return new HouseArea("House");
+else if (item.equals("Kitchen")) return new HouseArea("Kitchen");
+else if (item.equals("Bedroom")) return new HouseArea("Bedroom");
+else if (item.equals("Bathroom")) return new HouseArea("Bathroom");
+else if (item.equals("Downstairs")) return new HouseArea("Downstairs");
+else if (item.equals("Upstairs")) return new HouseArea("Upstairs");
+else return null;
+}
+}
